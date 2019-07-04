cf68043e7186f7bd1e1d4ad00a345d5d4a52df0b
mdinardo <mdinardo@users.noreply.github.com>
2019-07-04 17:19:57 -0400

    Update ieee_xplore_uw_proxy_redirect.user.js

--- ieee_xplore_uw_proxy_redirect.user.js
+++ ieee_xplore_uw_proxy_redirect.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Redirect IEEE Xplore to UW Library Proxy
-// @version 0.1
+// @version 0.2
 // @author Matthew Di Nardo
 // @updateURL https://raw.githubusercontent.com/mdinardo/userscripts/master/ieee_xplore_uw_proxy_redirect.user.js
 // @downloadURL https://raw.githubusercontent.com/mdinardo/userscripts/master/ieee_xplore_uw_proxy_redirect.user.js
@@ -11,6 +11,6 @@
 
 (function() {
     'use strict';
-    var newURL = location.href.replace(/ieeexplore.ieee.org/, newHost);
-    location.replace (newURL);
+    var newURL = location.href.replace(/ieeexplore.ieee.org/, "ieeexplore-ieee-org.proxy.lib.uwaterloo.ca");
+    location.replace(newURL);
 })();
